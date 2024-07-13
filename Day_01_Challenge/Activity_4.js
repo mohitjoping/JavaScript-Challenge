66a78a3078ec300fcd15f5e42f4d648c82984be6
Mohit Joping <118559559+mohitjoping@users.noreply.github.com>
2024-07-13 03:05:41 +0000

    Setup and Day_01 challenge is completed.

---
+++ Day_01_Challenge/Activity_4.js
@@ -0,0 +1,11 @@
+// Activity 4: Reassigning Variables
+
+// TASK 5
+
+// Declare a variable using let, assign it a initial value, reassign a new value, and log both vlaues to the console. 
+
+let CurrentCity = "Hisar"
+console.log(CurrentCity)
+
+CurrentCity = 'Nagaur'
+console.log(CurrentCity)
---
+++ Day_01_Challenge/Activity_5.js
@@ -0,0 +1,15 @@
+// Activity 4: UnderStanding const
+
+// TASK 6
+// Try reassigning a variable declared with const and observe the error.
+
+const id = 1234
+id = 1111
+
+// TypeError: Assignment to constant variable.
+// This will say assingning new value  in const is not allowed. So it give us a TypeError.
+
+let age = 12
+age = 13
+
+//In let we can reassign the value of variable within the scope But javascript doesn't allow it to do this with const.
